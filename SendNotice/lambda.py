ab98e0558b0fc8737b60bb1850b969e1d34e0c48
Cody Jenkins <iknowcss@gmail.com>
2020-09-28 22:37:34 +1000

    Add dynamo

--- SendNotice/lambda.py
+++ SendNotice/lambda.py
@@ -1,5 +1,15 @@
 import os
+import boto3
 
 def handler(event, context):
     PINPOINT_PROJECT_ID = os.environ.get('PINPOINT_PROJECT_ID')
-    print(PINPOINT_PROJECT_ID)
+    DISTRIBUTION_LIST_TABLE_NAME = os.environ.get('DISTRIBUTION_LIST_TABLE_NAME')
+    db = boto3.client('dynamodb')
+
+    try:
+        result = db.query(
+            TableName=DISTRIBUTION_LIST_TABLE_NAME
+        )
+        return {'Success': True, 'Items': result.Items}
+    except BaseException as e:
+        return {'Success': False, 'Error': str(e)}
