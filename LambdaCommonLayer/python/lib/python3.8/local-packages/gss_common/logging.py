7ce48f515580188a796d48b0d333d0e54c87f383
Cody Jenkins <iknowcss@gmail.com>
2020-10-08 09:32:19 +1100

    Make logging JSON only in lambda

--- LambdaCommonLayer/python/lib/python3.8/local-packages/gss_common/logging.py
+++ LambdaCommonLayer/python/lib/python3.8/local-packages/gss_common/logging.py
@@ -4,17 +4,17 @@ from pythonjsonlogger import jsonlogger
 
 LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
 
-if len(logging.getLogger().handlers) > 0:
-    logging.getLogger().setLevel(LOGLEVEL)
+root_logger = logging.getLogger()
+if len(root_logger.handlers) > 0:
+    root_logger.setLevel(LOGLEVEL)
+    root_logger.removeHandler(root_logger.handlers[0])
+    json_handler = logging.StreamHandler()
+    formatter = jsonlogger.JsonFormatter(fmt='%(asctime)s %(levelname)s %(name)s %(message)s')
+    json_handler.setFormatter(formatter)
+    root_logger.addHandler(json_handler)
 else:
     logging.basicConfig(level=LOGLEVEL)
 
+
 def get_common_logger(name):
-    logger = logging.getLogger(name)
-    json_handler = logging.StreamHandler()
-    formatter = jsonlogger.JsonFormatter(
-        fmt='%(asctime)s %(levelname)s %(name)s %(message)s'
-    )
-    json_handler.setFormatter(formatter)
-    logger.addHandler(json_handler)
-    return logger
+    return logging.getLogger(name)
