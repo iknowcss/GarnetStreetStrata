6608b9a0df7ca4d95eebd458d31f5abc657c3f3a
Cody Jenkins <cjenkins@dius.com.au>
2021-07-17 07:51:29 +1000

    Lint

--- src/shared/notification/domain/Resident.ts
+++ src/shared/notification/domain/Resident.ts
@@ -1,9 +1,12 @@
 import {Entity} from "../../domain/Entity";
+import {Result} from "../../core/Result";
 
-export interface ResidentProps {}
+export interface ResidentProps {
+  contactDetails: any[]
+}
 
 export class Resident extends Entity<ResidentProps> {
-  static create(props: ResidentProps) {
-    return new Resident(props);
+  static create(props: ResidentProps): Result<Resident> {
+    return Result.ok(new Resident(props));
   }
 }
